984ca8be06bbb54c0e57243af357f3a6d5220cf5
iralghamdi <reem2001gh@gmail.com>
2024-10-21 09:03:50 +0300

    Edit CSS file and add confg code

--- src/angular/app/app.config.ts
+++ src/angular/app/app.config.ts
@@ -1,13 +1,33 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
-
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
+import {
+  provideClientHydration,
+  withHttpTransferCacheOptions,
+} from '@angular/platform-browser';
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(),
+    provideAnimations(),
+    provideAnimationsAsync(),
+    provideRouter(routes),
+    provideClientHydration(
+      withHttpTransferCacheOptions({
+        includePostRequests: true,
+      })
+    ),
+    provideAnimationsAsync(),
   ],
 };
+ 
