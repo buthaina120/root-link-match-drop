50cc4fb7ff29021434893b58f47e69e4cfaf6bd8
Hanan Alharbi <61527960+Hanan-Abdullah@users.noreply.github.com>
2024-10-21 16:52:11 +0300

    add games list pages

--- src/angular/app/app.routes.ts
+++ src/angular/app/app.routes.ts
@@ -1,10 +1,10 @@
 import { Routes } from '@angular/router';
 import { SynonymsComponent } from './synonyms/synonyms/synonyms.component';
 import { RootsComponent } from './roots/roots/roots.component';
+import { GamesListComponent } from './games-list/games-list.component';
 
 export const routes: Routes = [
   { path: 'synonyms', component: SynonymsComponent },
   { path: 'roots', component: RootsComponent },
-
-  // { path: '', redirectTo: '/synonyms', pathMatch: 'full' },
+  { path: '', component: GamesListComponent },
 ];
