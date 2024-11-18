f9d1c884a73c05c368bea1dc9a2c426eac08bc15
Hanan Alharbi <61527960+Hanan-Abdullah@users.noreply.github.com>
2024-11-18 12:59:02 +0300

    edit

--- src/angular/app/app.config.ts
+++ src/angular/app/app.config.ts
@@ -1,17 +1,14 @@
-import {
-  ApplicationConfig,
-  importProvidersFrom,
-  provideZoneChangeDetection,
-} from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
+import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { routes } from './app.routes';
 import {
   provideClientHydration,
   withHttpTransferCacheOptions,
 } from '@angular/platform-browser';
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
