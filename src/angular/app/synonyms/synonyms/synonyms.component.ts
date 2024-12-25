de0f06dd378a13a457dadd99098ba91d14507b6b
iralghamdi <reem2001gh@gmail.com>
2024-12-25 16:04:40 +0300

    ملاحظات اجتماع الثلاثاء
    -فتح البطاقات يكون لبطاقتين فقط
    -ان يتناسب حجم الكلمات مع البطاقات بحيث لاتتعدى الكلمة حجم البطاقة
    -عنوان التحدي في الأعلى في المنتصف يكون بخط اوضح و اكبر
    -اضافة الصوت أحسنت نفس الموجود في لعبة جذر
    -عند عدم فوز اللاعب و انتهاء وقته يكون يظهر له دايلوق فيه نتائجه وليس فقط كلمة انتهى الوقت
    
    *****************************
    متبقي تعديلات اخرى غدا ان شاءالله

--- src/angular/app/synonyms/synonyms/synonyms.component.ts
+++ src/angular/app/synonyms/synonyms/synonyms.component.ts
@@ -1,11 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {
-  trigger,
-  state,
-  style,
-  transition,
-  animate,
-} from '@angular/animations';
+import {trigger,state,style,transition,animate,} from '@angular/animations';
 import { CommonModule } from '@angular/common';
 import { Howl } from 'howler';
 import { DialogModule } from 'primeng/dialog';
@@ -17,8 +11,6 @@ import { InputTextModule } from 'primeng/inputtext';
 import { ApiService } from '../../services/api.service';
 import { firstValueFrom } from 'rxjs';
 import { RouterModule } from '@angular/router'; 
-
-
 interface Card {
   id: number;
   word1: string;
@@ -26,7 +18,6 @@ interface Card {
   flipped: boolean;
   state?: string;
 }
-
 @Component({
   selector: 'app-synonyms',
   standalone: true,
@@ -65,39 +56,38 @@ export class SynonymsComponent implements OnInit {
   gameStarted: boolean = false;
   intervalId: any;
   difficulty: number = 1;
-  maxTime: number = 0;
+  maxTime: number = 60;
   timeLeft: number = this.maxTime;
   selectedDataType: string = '';
   gameType: string = 'مترادفات';
   selectedApi: 'synonyms' | 'antonyms' | 'translations' = 'synonyms';
   dataType: string = 'synonyms';
   selectedLevel: number = 0;
-  currentLevel: number = 1; // 1 = سهل، 2 = متوسط، 3 = صعب
+  currentLevel: number = 1;
   showLevelUpDialog: boolean = false;
-  playerName: string = ''; // متغير لتخزين اسم اللاعب
-  showPlayerNameDialog: boolean = false; // للتحكم في عرض الحوار
+  playerName: string = '';  
+  showPlayerNameDialog: boolean = false;   
   playerNameRequired: boolean =false;
+  lockBoard: boolean = false;
+  totalTimeSpent: number = 0;  
+
 
   openPlayerNameDialog() {
-    this.showPlayerNameDialog = true; // فتح الحوار عند النقر على زر ابدأ
+    this.showPlayerNameDialog = true;
   }
-  requiredMsg() {
-    // التحقق من إدخال الاسم
+  requiredMsg() {  
     if (!this.playerName.trim()) {
-     this.playerNameRequired = true; // إظهار رسالة الخطأ
+     this.playerNameRequired = true;   
    } else {
-     this.playerNameRequired = false; // إخفاء رسالة الخطأ
-     this.showPlayerNameDialog = false; // إغلاق الـ dialog
-     this.setflag(); // تنفيذ الإجراء المطلوب
+     this.playerNameRequired = false; 
+     this.showPlayerNameDialog = false;
+     this.setflag();  
    }
  }
   savePlayerName() {
-    this.showPlayerNameDialog = false; // إغلاق الحوار بعد حفظ اسم اللاعب
-    console.log('اسم اللاعب:', this.playerName); // استخدم الاسم كما تريد (طباعة أو حفظ)
+    this.showPlayerNameDialog = false;       
+    console.log('اسم اللاعب:', this.playerName); 
   }
-
-
-  
 setDataType(type: string) {
     this.selectedDataType = type;
     this.gameType =
@@ -108,27 +98,22 @@ setDataType(type: string) {
         : 'ترجمات';
     this.startGame();
   }
-
   setflag() {
     this.homeflag = false;
     this.levelflag = true;
   }
-
   showInstructions() {
     this.display = true;
   }
-
   selectLevel() {
     this.difficulty = this.selectedLevel;
     this.setTimerBasedOnLevel();
     this.startGame();
   }
-
   setTimerBasedOnLevel() {
-    this.maxTime = [50, 70, 90][this.difficulty - 1] || 60;
+    this.maxTime = [50,60,70][this.difficulty - 1];
     this.timeLeft = this.maxTime;
   }
-
   startGame() {
     const cardPairs = this.loadCardsBasedOnSelection();
     const selectedPairs = this.getPairsBasedOnDifficulty(cardPairs);
@@ -148,18 +133,21 @@ setDataType(type: string) {
         },
       ])
       .flat();
-
+  
     this.cards = uniqueCards;
     this.shuffleCards();
     this.points = 0;
     this.attempts = 0;
     this.matchedCards = [];
     this.flippedCards = [];
+  
+    this.totalTimeSpent = 0;        
     this.startTimer();
+  
     this.levelflag = false;
     this.gameflag = true;
   }
-
+  
   private loadCardsBasedOnSelection() {
     switch (this.selectedDataType) {
       case 'synonyms':
@@ -214,12 +202,14 @@ setDataType(type: string) {
   ) {
     return cardPairs.slice(0, this.difficulty * 2 + 4);
   }
-
   startTimer() {
     clearInterval(this.intervalId);
+  
     this.intervalId = setInterval(() => {
       if (this.timeLeft > 0) {
         this.timeLeft -= 1;
+        this.totalTimeSpent += 1; 
+  
         if (this.timeLeft === 10) {
           this.warningSound.play();
         }
@@ -232,7 +222,6 @@ setDataType(type: string) {
       this.checkForWin();
     }, 1000);
   }
-
   checkForWin() {
     if (this.matchedCards.length === this.cards.length) {
       clearInterval(this.intervalId);
@@ -241,78 +230,78 @@ setDataType(type: string) {
     }
   }
   displayWinMessage() {
+    this.warningSound.stop(); 
     this.tadaSound.play();
-    this.showWinDialog = true; // عرض حوار الفوز
-  }
+    this.showWinDialog = true;  
+    clearInterval(this.intervalId);   
+    console.log('الوقت المستغرق للمستوى الحالي:', this.totalTimeSpent);   }
+  
   nextLevel() {
     if (this.currentLevel < 3) {
-      this.currentLevel += 1; // الترقية للمستوى التالي
-      this.selectedLevel = this.currentLevel; // تحديث المستوى المختار
-      this.setDataType(this.selectedDataType); // تأكيد نوع التحدي
-      this.selectLevel(); // بدء اللعبة بالمستوى الجديد
+      this.currentLevel += 1;
+      this.selectedLevel = this.currentLevel;
+      this.setDataType(this.selectedDataType);
+      this.totalTimeSpent = 0;       
+      this.selectLevel();
     }
   }
   shuffleCards() {
     this.cards.sort(() => Math.random() - 0.5);
   }
-
   flipCard(card: Card) {
     if (!this.intervalId) {
       this.startTimer();
     }
+  
     if (
-      this.flippedCards.length < 2 &&
-      !this.flippedCards.includes(card) &&
-      !this.matchedCards.includes(card)
+      this.lockBoard ||
+      this.flippedCards.length >= 2 ||  
+      this.flippedCards.includes(card) ||  
+      this.matchedCards.includes(card)
     ) {
-      card.flipped = true;
-      this.flippedCards.push(card);
-      if (this.flippedCards.length === 2) {
-        if (
-          this.flippedCards[0].matchingWord !== this.flippedCards[1].word1 &&
-          this.flippedCards[0].word1 !== this.flippedCards[1].matchingWord
-        ) {
-          this.attempts += 1; // زيادة عدد المحاولات فقط إذا كانت غير صحيحة
-        }
-        this.checkMatch();
-      }
+      return;
+    }
+  
+     
+    card.flipped = true;
+    this.flippedCards.push(card);
+  
+    if (this.flippedCards.length === 2) {
+      this.lockBoard = true; 
+      this.checkMatch();
     }
   }
-  
   checkMatch() {
     const [firstCard, secondCard] = this.flippedCards;
+  
     if (
       firstCard.word1 === secondCard.matchingWord ||
       firstCard.matchingWord === secondCard.word1
     ) {
       this.matchedCards.push(firstCard, secondCard);
       this.points += 1;
-
       this.correctSound.play();
-
-      // تأخير إخفاء البطاقات
+  
       setTimeout(() => {
-        firstCard.flipped = false;
-        secondCard.flipped = false;
-
-        // تطبيق حالة 'matched' على البطاقات لإخفائها
         firstCard['state'] = 'matched';
         secondCard['state'] = 'matched';
-      }, 3000); // تأخير لمدة 1000 مللي ثانية (1 ثانية)
-
-      this.checkForWin();
+        this.resetBoard();
+      }, 1000);
     } else {
+      this.attempts += 1;  
       this.wrongSound.play();
-
-      // تأخير إغلاق البطاقات في حالة الخطأ
+  
       setTimeout(() => {
         firstCard.flipped = false;
         secondCard.flipped = false;
-      }, 1000); // تأخير لمدة 1000 مللي ثانية (1 ثانية)
+        this.resetBoard();
+      }, 1000);
     }
-    this.flippedCards = [];
   }
-
+  resetBoard() {
+    this.flippedCards = [];
+    this.lockBoard = false; 
+  }
   closeDialog() {
     this.showDialog = false;
     this.showWinDialog = false;
@@ -344,12 +333,21 @@ setDataType(type: string) {
     this.flippedCards = [];
     this.matchedCards = [];
     this.selectedLevel = this.currentLevel;
+    this.totalTimeSpent = 0;   
     this.setTimerBasedOnLevel();
     this.startGame();
     this.showDialog = false;
     this.showWinDialog = false;
     this.showLevelUpDialog = false;
   }
+  resumeGame() {
+    const extraTime = 30;     
+    this.timeLeft += extraTime;  
+    this.showDialog = false; 
+    this.warningSound.stop();  
+    this.startTimer();  
+  }
+  
   getStarsCount(): number {
     const totalCards = this.cards.length;
     const matchedCardsCount = this.matchedCards.length;
@@ -358,7 +356,6 @@ setDataType(type: string) {
     else if (matchedCardsCount > 0) return 1;
     else return 0;
   }
-
   goBack() {
     this.gameflag = false;
     this.levelflag = true;
@@ -368,11 +365,14 @@ setDataType(type: string) {
   }
   restartToLevelFlag() {
     this.showWinDialog=false;
-    this.gameflag = false; // إخفاء واجهة اللعبة
-    this.levelflag = true; // عرض صفحة اختيار المستوى
-    this.homeflag = false; // تأكيد أن صفحة البداية مخفية
-    clearInterval(this.intervalId); // إيقاف المؤقت
-    this.timeLeft = this.maxTime; // إعادة ضبط الوقت
+    this.gameflag = false;
+    this.levelflag = true;  
+    this.homeflag = false; 
+    clearInterval(this.intervalId);  
+    this.timeLeft = this.maxTime;  
   }
-
+  ngOnDestroy() {
+    this.warningSound.stop();
+    clearInterval(this.intervalId);   
+  }
 }
